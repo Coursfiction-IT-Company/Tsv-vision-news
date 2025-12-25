e14a9ccae4938f5bde1d38e415d82f9f56ee167f
Ayas Ibrahim <aayasabraham@gmail.com>
2025-12-26 00:26:53 +0600

    refactor: improve code readability and structure in SportsSection component

--- components/SportsSection.tsx
+++ components/SportsSection.tsx
@@ -8,29 +8,29 @@ import SectionHeader from "./ui/SectionHeader";
 import FullScreenLoading from "./ui/FullScreenLoading";
 import { useGetNewsQuery } from "@/app/redux/features/news/newsApi";
 
-
 const SportsSection: React.FC = () => {
   const { data, isLoading, error } = useGetNewsQuery({
     category: "খেলা",
     limit: 10,
   });
-  const news=data?.data||[];
+  const news = data?.data || [];
 
   if (!news.length) return null;
- if (error) return <p className="text-center py-10 text-red-400">Failed to load news.</p>;
+  if (error)
+    return (
+      <p className="text-center py-10 text-red-400">Failed to load news.</p>
+    );
   // Distribute Layout Sections
-  const leftTwo = news.slice(0, 2);         // Left 2 articles
-  const centerMain = news[2];               // Center main article
-  const rightSmall = news.slice(3);         // Remaining right column
+  const leftTwo = news.slice(0, 3); // Left 2 articles
+  const centerMain = news[2]; // Center main article
+  const rightSmall = news.slice(3,6); // Remaining right column
 
   return (
     <section className="bg-white">
       <div className="p-4 sm:p-8 container mx-auto">
-        
         <SectionHeader title="খেলা" />
-  {isLoading && <FullScreenLoading/>}
+        {isLoading && <FullScreenLoading />}
         <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-          
           {/* LEFT COLUMN — 2 stacked cards */}
           <div className="lg:col-span-3 flex flex-col gap-6">
             {leftTwo.map((article) => (
@@ -66,7 +66,11 @@ const SportsSection: React.FC = () => {
               {rightSmall.map((article, index) => (
                 <div
                   key={article._id}
-                  className={index < rightSmall.length - 1 ? "border-b border-gray-100" : ""}
+                  className={
+                    index < rightSmall.length - 1
+                      ? "border-b border-gray-100"
+                      : ""
+                  }
                 >
                   <RightSmallHorizontalCard
                     article={{
@@ -80,7 +84,6 @@ const SportsSection: React.FC = () => {
               ))}
             </div>
           </div>
-
         </div>
       </div>
     </section>
